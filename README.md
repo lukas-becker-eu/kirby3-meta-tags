b90f069e65b6f34eff65745bacf2f6faebfaab73
Lukas Becker <lukas@candyblue.agency>
2020-02-13 16:11:04 +0100

    update main project structure

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # kirby-v3-head
 
 ## Installation
-```$ git submodule add https://github.com/wearecandyblue/kirby-v3-head.git site/plugins ```
+```$ git submodule add https://github.com/wearecandyblue/kirby-v3-head.git site/plugins/head ```
+
+## Updating
+```
+$ cd site/plugins/head
+$ git checkout master
+$ git pull
+```
---
+++ snippets/head.php
@@ -0,0 +1,13 @@
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+  <!-- The title tag we show the title of our site and the title of the current page -->
+  <title><?= $site->title() ?> | <?= $page->title() ?></title>
+
+  <!-- Stylesheets can be included using the `css()` helper. Kirby also provides the `js()` helper to include script file.
+        More Kirby helpers: https://getkirby.com/docs/reference/templates/helpers -->
+  <?= css(['assets/css/index.css', '@auto']) ?>
+
+</head>
